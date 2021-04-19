94b2137d8fead1a4f962586e0c148eebe06a58f8
epidia <clairon_6@hotmail.fr>
2021-04-19 07:35:22 +0200

    Mise a jour du code

--- routes/offer.js
+++ routes/offer.js
@@ -54,9 +54,15 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
         owner: req.user,
       });
       let pictureToUpload = req.files.picture.path;
-      const result = await cloudinary.uploader.upload(pictureToUpload, {
-        folder: `/vinted/offers/${newOffer._id}`,
-      });
+      const result = await cloudinary.uploader.unsigned_upload(
+        pictureToUpload,
+        "vinted_upload",
+        {
+          folder: `api/vinted/offers/${newOffer._id}`,
+          public_id: "preview",
+          cloud_name: "my-project-vinted",
+        }
+      );
       newOffer.product_image = result;
       await newOffer.save();
       res.status(200).json({
@@ -128,80 +134,93 @@ router.get("/offers", async (req, res) => {
   }
 });
 
-router.put("/offer/update", isAuthenticated, async (req, res) => {
+router.put("/offer/update/:id", isAuthenticated, async (req, res) => {
+  const offerToModify = await Offer.findById(req.params.id);
   try {
-    const offer = await Offer.findById(req.fields.id);
-    const {
-      name,
-      description,
-      price,
-      brand,
-      etat,
-      size,
-      color,
-      location,
-    } = req.fields;
-    if (!offer) {
-      res.status(400).json("This offer doesnt exist.");
-    } else {
-      let pictureToUpload = req.files.picture.path;
-      const result = await cloudinary.uploader.upload(pictureToUpload, {
-        folder: "/vinted/offers",
-      });
-      offer.product_name = name;
-      offer.product_description = description;
-      offer.product_price = price;
-      offer.product_details = [
-        { brand },
-        { etat },
-        { size },
-        { color },
-        { location },
-      ];
-      offer.product_image = result;
-      await offer.save();
-      res.status(200).json({
-        _id: offer._id,
-        product_name: offer.product_name,
-        product_description: offer.product_description,
-        product_price: offer.product_price,
-        product_details: offer.product_details,
-        product_image: result.secure_url,
-        owner: {
-          id: req.user.id,
-          account: req.user.account,
-        },
-      });
+    if (req.fields.title) {
+      offerToModify.product_name = req.fields.title;
     }
+    if (req.fields.description) {
+      offerToModify.product_description = req.fields.description;
+    }
+    if (req.fields.price) {
+      offerToModify.product_price = req.fields.price;
+    }
+
+    const details = offerToModify.product_details;
+    for (i = 0; i < details.length; i++) {
+      if (details[i].MARQUE) {
+        if (req.fields.brand) {
+          details[i].MARQUE = req.fields.brand;
+        }
+      }
+      if (details[i].TAILLE) {
+        if (req.fields.size) {
+          details[i].TAILLE = req.fields.size;
+        }
+      }
+      if (details[i].ÉTAT) {
+        if (req.fields.condition) {
+          details[i].ÉTAT = req.fields.condition;
+        }
+      }
+      if (details[i].COULEUR) {
+        if (req.fields.color) {
+          details[i].COULEUR = req.fields.color;
+        }
+      }
+      if (details[i].EMPLACEMENT) {
+        if (req.fields.location) {
+          details[i].EMPLACEMENT = req.fields.location;
+        }
+      }
+    }
+
+    // Notifie Mongoose que l'on a modifié le tableau product_details
+    offerToModify.markModified("product_details");
+
+    if (req.files.picture) {
+      const result = await cloudinary.uploader.upload(req.files.picture.path, {
+        public_id: `api/vinted/offers/${offerToModify._id}/preview`,
+      });
+      offerToModify.product_image = result;
+    }
+
+    await offerToModify.save();
+
+    res.status(200).json("Offer modified succesfully !");
   } catch (error) {
+    console.log(error.message);
     res.status(400).json({ error: error.message });
   }
 });
 
-router.delete("/offer/delete", isAuthenticated, async (req, res) => {
+router.delete("/offer/delete/:id", isAuthenticated, async (req, res) => {
   try {
-    const offerToDelete = await Offer.findById(req.query.id);
-    if (offerToDelete) {
-      offerToDelete.delete();
-      res.status(200).json("The offer has been deleted.");
-    } else {
-      res.status(200).json("The offer you want to delete doesnt exist.");
-    }
+    //Je supprime ce qui il y a dans le dossier
+    await cloudinary.api.delete_resources_by_prefix(
+      `api/vinted/offers/${req.params.id}`
+    );
+    //Une fois le dossier vide, je peux le supprimer !
+    await cloudinary.api.delete_folder(`api/vinted/offers/${req.params.id}`);
+
+    offerToDelete = await Offer.findById(req.params.id);
+
+    await offerToDelete.delete();
+
+    res.status(200).json("Offer deleted succesfully !");
   } catch (error) {
+    console.log(error.message);
     res.status(400).json({ error: error.message });
   }
 });
 
 router.get("/offer/:id", async (req, res) => {
   try {
-    // const offer = await Offer.findById(req.params.id).populate(
-    //   "owner",
-    //   "account.username"
-    // );
-    const offer = await Offer.findById(req.params.id).populate({
-      path: "owner",
-      select: "account.username",
-    });
+    const offer = await Offer.findById(req.params.id).populate(
+      "owner",
+      "account"
+    );
     res.json(offer);
   } catch (error) {
     res.status(400).json({ error: error.message });
